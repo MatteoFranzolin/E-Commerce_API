4e1eb9a380827929a834cee482a66591edf68417
MatteoFranzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2024-02-18 21:31:22 +0100

    aggiunto controller products e gestione creazione prodotti

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+class ProductController {
+    public function add() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nome'], $_POST['descrizione'], $_POST['prezzo'])) {
+            $product = Product::Create($_POST);
+            if ($product) {
+                header('Location: products.php?success=1');
+            } else {
+                header('Location: products.php?error=1');
+            }
+        } else {
+            header('Location: add_product.php');
+        }
+        exit();
+    }
+}
