586db15a1ffd381716618ccdfd8cebf5f7ae0e61
MatteoFranzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2024-02-18 21:41:18 +0100

    aggiunto metodo view

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -16,4 +16,19 @@ class ProductController
         }
         exit();
     }
+
+    public function view()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $products = Product::FetchAll();
+            if ($products) {
+                header('Location: products.php?success=1');
+            } else {
+                header('Location: products.php?error=1');
+            }
+        } else {
+            header('Location: products.php');
+        }
+        exit();
+    }
 }
