30827fdf85ab8059b5e148dbc1e39d9c6e9b029e
MatteoFranzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2024-03-20 22:40:56 +0100

    corretto CORS

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *'); // Consenti richieste da qualsiasi origine
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); // Consenti metodi HTTP specifici
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'); // Consenti metodi HTTP specifici
 header('Access-Control-Allow-Headers: Content-Type'); // Consenti header specifici
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
